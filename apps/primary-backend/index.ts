2f16795635f03b1890779c2d3613b80af563dd8c
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-06-09 08:48:34 +0530

    feat(auth): implement JWT authentication middleware and associate projects with users

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,28 +1,30 @@
 import { prismaClient } from "db/client";
 import express from "express";
 import cors from "cors";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
-app.post("/project", async (req, res) => {
+app.post("/project",authMiddleware, async (req, res) => {
   const { prompt } = req.body;
-
+    const userId = req.userId!
   //TODO: add logic to get a useful name for the project from the prompt
   const description = prompt.split("\ n")[0];
   const project = await prismaClient.project.create({
     data: {
-      description,
+      description, userId
     },
   });
   res.json({projectId: project.id});
 });
 
-app.get("/projects", async (req, res) => {
-    const projects = await prismaClient.project.findUnique({
-        where: {id: projectId },
+app.get("/projects", authMiddleware, async (req, res) => {
+    const userId = req.userId!
+    const projects = await prismaClient.project.findFirst({
+        where: { userId },
     })
     res.json({ projects });
 })
